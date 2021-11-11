f900019a4f5203e19f4be142f0866087544022a7
Mateus Kahler <mateus.kahler@gmail.com>
2021-11-11 20:57:05 -0300

    resolução tarefa 1

--- leitura.py
+++ leitura.py
@@ -1,2 +1,15 @@
+import pandas as pd
+from pathlib import Path
+
+import os
+
+
+def criar_caminho_arquivo(nome_arquivo):
+    PASTA_DOS_DADOS = 'dados'
+
+    return os.path.join('.', PASTA_DOS_DADOS, nome_arquivo)
+
+
 def ler_arquivos():
-    return None
+    caminho_arquivo = criar_caminho_arquivo('1_demografico.csv')
+    return pd.read_csv(caminho_arquivo, sep=';')
