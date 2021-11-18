2dec58a29dc09c32ac348313f338745163d0c2f2
Hugo Martins <hugoplm@Hugos-MacBook-Air.local>
2021-11-18 20:47:28 -0300

    incluindo docstring nas funções

--- dummies.py
+++ dummies.py
@@ -2,6 +2,16 @@
 import pandas as pd
 
 def criar_dummies(input_df, colunas, df_completo=False):
+    '''
+    Parametros:
+        input_df (dataframe): dataframe de entrada
+        colunas (list): lista com os nomes das colunas para criar dummies
+        df_completo: quando False, retorna apenas as colunas dummies criadas,
+            quando True, retorna o dataframe completo com as colunas dummies
+    
+    Retorno:
+        df_dummies (dataframe): dataframe com colunas dummies
+    '''
     df = input_df.copy()
     
     if df_completo == False:
