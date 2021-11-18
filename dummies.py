867fac5fc7d36d538a1eb7c210e1a92238d3ae0e
Hugo Martins <hugoplm@Hugos-MacBook-Air.local>
2021-11-18 19:38:58 -0300

    incluindo notebook

--- dummies.py
+++ dummies.py
@@ -1,10 +1,12 @@
 
+import pandas as pd
+
 def criar_dummies(input_df, colunas, df_completo=False):
     df = input_df.copy()
     
-    if retornar_completo == False:
+    if df_completo == False:
         df_dummies = pd.get_dummies(df[colunas])
-    elif retornar_completo == True:
+    elif df_completo == True:
         df.drop(colunas, axis=1)
         df_dummies = df.merge(pd.get_dummies(df[colunas]), left_index=True, right_index=True)
     
